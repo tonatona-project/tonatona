e9e8cccbae4ff34e9d8cae179381cb286c88c601
(cdep)illabout <cdep.illabout@gmail.com>
2018-06-20 12:28:32 +0900

    Add some helper functions to Tonatona.

--- tonatona/src/Tonatona.hs
+++ tonatona/src/Tonatona.hs
@@ -2,12 +2,18 @@
 
 module Tonatona
   ( run
+  , runWithConf
+  , runWithConfAndShared
+  , readerConf
+  , readerShared
+  , askConf
+  , askShared
   , TonaM
   , Plug(..)
   , lift
   ) where
 
-import Control.Monad.Reader (ReaderT, runReaderT)
+import Control.Monad.Reader (ReaderT, runReaderT, reader)
 import Control.Monad.Trans (lift)
 import Data.Semigroup ((<>))
 import System.Envy (FromEnv, decodeEnv)
@@ -25,9 +31,27 @@ run action = do
   mconf <- decodeEnv
   case mconf of
     Left err -> error $ "Fail to decode env: " <> err
-    Right conf -> do
-      shared <- Tonatona.init conf
-      runReaderT action (conf, shared)
+    Right conf -> runWithConf conf action
+
+runWithConf :: Plug conf shared => conf -> TonaM conf shared a -> IO a
+runWithConf conf action = do
+  shared <- Tonatona.init conf
+  runWithConfAndShared conf shared action
+
+runWithConfAndShared :: conf -> shared -> TonaM conf shared a -> IO a
+runWithConfAndShared conf shared action = runReaderT action (conf, shared)
+
+readerConf :: (conf -> a) -> TonaM conf shared a
+readerConf f = reader (f . fst)
+
+readerShared :: (shared -> a) -> TonaM conf shared a
+readerShared f = reader (f . snd)
+
+askConf :: TonaM conf shared conf
+askConf = readerConf id
+
+askShared :: TonaM conf shared shared
+askShared = readerShared id
 
 {-| A type class for configuration.
  - The 'config' is supposed to be an instance of 'FromEnv'.
