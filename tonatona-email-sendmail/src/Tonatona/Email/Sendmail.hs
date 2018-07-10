2918a8bea4472f86bfdf9f8f767978e6cd362631
(cdep)illabout <cdep.illabout@gmail.com>
2018-07-10 12:04:07 +0900

    Change the tonatona-email-sendmail module to just exporting the send function which wraps the Network.Mail.Mime.renderSendMail function.

--- tonatona-email-sendmail/src/Tonatona/Email/Sendmail.hs
+++ tonatona-email-sendmail/src/Tonatona/Email/Sendmail.hs
@@ -1,16 +1,12 @@
 module Tonatona.Email.Sendmail
   ( send
-  , Tonatona.Email.Sendmail.init
-  , TonaEmailShared(..)
-  , Shared(..)
   , module Network.Mail.Mime
-  -- , NetworkMail.simpleMail'
   ) where
 
 import Control.Monad.IO.Class (liftIO)
 import Network.Mail.Mime
 
-import Tonatona.Email (Shared(..), TonaEmailShared(..), send)
+import Tonatona (TonaM)
 
-init :: IO Shared
-init = pure $ Shared (liftIO . renderSendMail)
+send :: Mail -> TonaM conf shared ()
+send = liftIO . renderSendMail
