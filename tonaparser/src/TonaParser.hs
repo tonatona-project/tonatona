0ffa5355812b0a59daa0211ba23eaaca3e8197ea
(cdep)illabout <cdep.illabout@gmail.com>
2018-08-07 16:18:19 +0900

    Get everything to compile.

--- tonaparser/src/TonaParser.hs
+++ tonaparser/src/TonaParser.hs
@@ -6,8 +6,12 @@ import Control.Monad.Except (ExceptT, MonadError)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Reader (ReaderT, runReaderT, reader)
 import Control.Monad.Trans (lift)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Semigroup ((<>))
-import System.Envy (FromEnv, decodeEnv)
+import System.Environment (getArgs, getEnvironment)
+import System.Envy (Var(fromVar))
 
 {-| Example
 
@@ -45,33 +49,117 @@ import System.Envy (FromEnv, decodeEnv)
       <*> barWithPrefix
  -}
 
+-- data Foo = Foo
+--   { foo :: Int
+--   , bar :: Bar
+--   }
+
+data Bar = Bar
+  { baz :: String
+  }
+
+-- If environment variable "BAZ" exist, use the value
+-- else if command line argument "--baz" exist, use the value
+-- else if command line argument "-b" exist, use the value
+-- else use default value "baz"
+instance FromEnv Bar where
+  fromEnv = Bar
+    <$> envDef (envVar "BAZ" .|| argLong "baz" .|| argShort 'b') "baz"
+
+-- If environment variable "BAR_BAZ" exist, use the value
+-- else if command line argument "--bar-baz" exist, use the value
+-- else use default value "baz"
+-- ( short hands argument "-b" was deactivated )
+-- barWithPrefix :: Parser a
+-- barWithPrefix =
+--   modifyConfig $
+--     modifyEnvVarKey ("BAR_" <>) .
+--     modifyArgLongKey ("bar-" <>) .
+--     modifyArgShortKey const Nothing
+
+-- FromEnv Foo where
+--   fromEnv = Foo
+--     <$> env (envVar "FOO" .|| argLong "foo")
+--     <*> barWithPrefix
 
 
 class FromEnv a where
   fromEnv :: Parser a
 
+decodeEnv :: FromEnv a => IO (Maybe a)
+decodeEnv = do
+  let parser = fromEnv
+  envVars <- getEnvVars
+  cmdLineArgs <- getCmdLineArgs
+  pure $ runParser parser envVars cmdLineArgs
+
+runParser :: Parser a -> Map String String -> [(String, String)] -> Maybe a
+runParser (Parser parserFunc) envVars cmdLineArgs =
+  let conf =
+        Config
+          { confCmdLineArgs = cmdLineArgs
+          , confEnvVars = envVars
+          }
+  in parserFunc conf
+
+getEnvVars :: IO (Map String String)
+getEnvVars = do
+  environment <- getEnvironment
+  pure $ Map.fromList environment
+
+-- TODO: Does not properly handle command line arguments.
+-- Probably should use parsing code from something like optparse-applicative.
+getCmdLineArgs :: IO [(String, String)]
+getCmdLineArgs = do
+  args <- getArgs
+  pure $ tupleList args
+  where
+    -- This throws away the last time if the input list is not even.
+    tupleList :: [a] -> [(a,a)]
+    tupleList (a:b:cs) = (a, b) : tupleList cs
+    tupleList _ = []
+
+env :: Var a => Source -> Parser a
+env (Source srcs) =
+  Parser $ \conf -> do
+    -- find matching source
+    val <- findValInSrc conf srcs
+    fromVar val
+
+findValInSrc :: Config -> [InnerSource] -> Maybe String
+findValInSrc conf srcs = listToMaybe $ mapMaybe (findValInSrcs conf) srcs
+
+findValInSrcs :: Config -> InnerSource -> Maybe String
+findValInSrcs (Config cmdLineArgs _) (ArgLong str) = lookup str cmdLineArgs
+findValInSrcs (Config cmdLineArgs _) (ArgShort ch) = lookup [ch] cmdLineArgs
+findValInSrcs (Config _ envVars) (EnvVar var) = Map.lookup var envVars
+
+envDef :: Var a => Source -> a -> Parser a
+envDef src df = Parser $ \conf ->
+  let Parser f = env src
+  in
+  case f conf of
+    Nothing -> Just df
+    Just a -> Just a
+
 newtype Parser a = Parser
-  { getConfig :: Config a
+  { getConfig :: Config -> Maybe a
   }
-  -- deriving ( Functor
-  --            , Monad
-  --            , Applicative
-  --            , MonadError String
-  --            , MonadIO
-  --            , Alternative
-  --            , MonadPlus
-  --            )
-
-
-runParser :: ExceptT String IO a
-runParser = undefined
+  deriving (Functor
+             -- , Monad
+             -- , Applicative
+             -- , MonadError String
+             -- , MonadIO
+             -- , Alternative
+             -- , MonadPlus
+             )
 
 
 {-| Opaque type.
  -}
-data Config a = Config
-  { defaultValue :: Maybe a
-  , sources :: [InnerSource]
+data Config = Config
+  { confCmdLineArgs :: [(String, String)]
+  , confEnvVars :: Map String String
   }
 
 
@@ -91,3 +179,9 @@ newtype Source = Source { unSource :: [InnerSource] }
 envVar :: String -> Source
 envVar name =
   Source [EnvVar name]
+
+argLong :: String -> Source
+argLong name = Source [ArgLong name]
+
+argShort :: Char -> Source
+argShort name = Source [ArgShort name]
